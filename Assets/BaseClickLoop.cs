afbe26f2b14cc8e24882f003dfe58aad95d9ed72
ArTeGaS-2 <probablytorvik@gmail.com>
2025-09-22 19:56:30 +0300

    Додали метод ефекту натискання кнопки, індикатор стану, та метод відпускання кнопки.

--- Assets/BaseClickLoop.cs
+++ Assets/BaseClickLoop.cs
@@ -7,10 +7,24 @@ public class BaseClickLoop : MonoBehaviour
 {
     public int clickCounter = 0; // Змінна лічильника
     public TextMeshProUGUI textObj; // Об'єкт тексту на сцені
+    public bool buttonPressStatus = false; // Чи натиснута кнопка
 
     private void OnMouseDown()
     {
+        ClickButton();
+        ClickImpactEffect();
+    }
+    private void ClickButton()
+    {
         clickCounter++;
-        textObj.text = "Монет: " + clickCounter.ToString(); 
+        textObj.text = "Монет: " + clickCounter.ToString();
+    }
+    private void ClickImpactEffect()
+    {
+        transform.localPosition = new Vector3(0, 0.25f, 0); // 0  0.5  0
+    }
+    private void ClickImpactEffectDisable()
+    {
+        transform.localPosition = new Vector3(0, 0.5f, 0);
     }
 }
