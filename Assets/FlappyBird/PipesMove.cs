71c2ff80c079d4b76d31bc2314090729dc90c4ff
ArTeGaS-2 <probablytorvik@gmail.com>
2025-12-15 20:18:35 +0200

    Префаб труб і їх рух. Стикання і перезапуск сцени

---
+++ Assets/FlappyBird/PipesMove.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipesMove : MonoBehaviour
+{
+    public float speed = 5f;
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector3.back * speed * Time.fixedDeltaTime);
+    }
+}
--- Assets/FlappyBird/Player_Controller.cs
+++ Assets/FlappyBird/Player_Controller.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player_Controller : MonoBehaviour
 {
+    public GameObject camera; 
     private Rigidbody rb; // фізичний компонент
+
+
+
     public float jumpVelocity = 5f; // сила стрибка
-    public float forwardVelocity = 5f; // сила руху вперед
 
     private void Start()
     {
@@ -18,15 +22,19 @@ public class Player_Controller : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) ||
             Input.GetMouseButtonDown(0))
         {
-            rb.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
+            rb.AddForce(Vector3.up * jumpVelocity,
+                ForceMode.Impulse);
         }
     }
-    private void FixedUpdate()
+    private void LateUpdate()
     {
-        // рух вперед
-        transform.Translate(
-            Vector3.forward * forwardVelocity * Time.fixedDeltaTime);
-        Debug.Log(rb.velocity.y);
+        camera.transform.position = new Vector3(
+            camera.transform.position.x,
+            transform.position.y,
+            camera.transform.position.z);
     }
-
+    private void OnCollisionEnter(Collision collision)
+    {
+        SceneManager.LoadScene(1);
+    }
 }
