51e365ed031882dac82fef44d15012a8976655bd
ArTeGaS-2 <probablytorvik@gmail.com>
2026-01-12 20:04:14 +0200

    Додано генерацію кастомних варіантів труб

--- Assets/FlappyBird/LvL_Gen.cs
+++ Assets/FlappyBird/LvL_Gen.cs
@@ -4,25 +4,45 @@ using UnityEngine;
 
 public class LvL_Gen : MonoBehaviour
 {
+    [Header("Шаблони труб")]
     public GameObject pipesPrefab; // шаблон труб (налаштований зразок)
+    public List<GameObject> listOfAnotherPrefabs; // інші префаби
+
+    [Header("Загальні налаштування")]
     [Range(0f, 5f)] public float heightRange; // діапазон висоти труб
     [Range(0.1f, 5f)] public float spawnInterval; // інтервал створення
-    public float retreat = 3f; // Відступ за межі екрану
+    public int cycleCounter = 3; // підрахунок труб
+
+    private int listCounter = 0; // пам'ять про номер останнього шаблону
 
     private IEnumerator SpawnPipes()
     {
         Vector3 pos = transform.position;
         float height = 0f;
+        float currentCycleNum = 0f;
 
         while (true)
         {
             pos.y = Random.Range(-heightRange, heightRange);
 
-            Instantiate( // Створити об'єкт
-            pipesPrefab, // шаблон об'єкту
-            pos, // положення
-            Quaternion.identity); // обертання
+            if (currentCycleNum > cycleCounter)
+            {
+                Instantiate( // Створити об'єкт
+                listOfAnotherPrefabs[0], // шаблон об'єкту
+                pos, // положення
+                Quaternion.identity); // обертання
 
+                currentCycleNum = 0;
+            }
+            else
+            {
+                Instantiate( // Створити об'єкт
+               pipesPrefab, // шаблон об'єкту
+               pos, // положення
+               Quaternion.identity); // обертання
+
+                currentCycleNum++;
+            }
             yield return new WaitForSeconds(spawnInterval);
         }
     }
