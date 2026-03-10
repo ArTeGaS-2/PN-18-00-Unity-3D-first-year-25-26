ff83e9b5ebbcdb949530455dc2404b9945bc6a49
ArTeGaS-2 <probablytorvik@gmail.com>
2026-03-10 15:01:24 +0200

    Змінили план, переорієнтувавшись на Tower Defence замість арени. Змінили базовий макет мапи і почали роботу з AI Navigation.

---
+++ Assets/Arena/EnemyAgent.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAgent : MonoBehaviour
+{
+    [SerializeField] string targetName; // Назва цілі на сцені
+    [SerializeField] float enemyMaxHP = 20f; // Максимальне здоров'я ворога
+    [SerializeField] float enemyDamage = 5f; // Сила атаки ворога
+
+    private float enemyCurrentHP; // Поточне здоров'я ворога
+    private NavMeshAgent agent; // Компонент NavMeshAgent для руху ворога
+
+    private void Start()
+    {
+        enemyCurrentHP = enemyMaxHP; // Ініціалізуємо поточне здоров'я ворога
+        agent = GetComponent<NavMeshAgent>(); // Отримуємо компонент NavMeshAgent
+
+        agent.SetDestination(
+            GameObject.Find(targetName // Знаходимо об'єкт з назвою targetName
+            ).transform.position); // Встановлюємо ціль для руху ворога
+    }
+}
